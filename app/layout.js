08212fa952f6d496b3fd4d2b2fc6b1cb117965b9
Tony Kanyamuka <tonykanyamuka@gmail.com>
2024-08-08 18:06:41 +0200

    create a header,nav,motion transitions:use framer-motion,create page templates

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,15 @@
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/Header";
 
-const jetbrains_Mono = JetBrains_Mono({ subsets: ["latin"],weight:['100','200','300','400','500','600','700','800'],variable:'--font-jetbrainsMono' });
+import Header from "@/components/Header";
+import PageTransition from "@/components/PageTransition";
+import StairTransition from "@/components/StairTransition";
+
+const jetbrains_Mono = JetBrains_Mono({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+  variable: "--font-jetbrainsMono",
+});
 
 export const metadata = {
   title: "Portofolio V2",
@@ -12,8 +19,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Header/>
-      <body className={jetbrains_Mono.variable}>{children}</body>
+      <body className={jetbrains_Mono.variable}>
+        <Header />
+        <StairTransition />
+        <PageTransition>{children}</PageTransition>
+      </body>
     </html>
   );
 }
